bacc485edb7c0ae3eb9cddf7a5a1e654126fab85
Anna Stanek <aniastanekk@gmail.com>
2020-08-11 23:08:36 +0200

    Lesson 42 - Java Script Timers

--- new.js
+++ new.js
@@ -98,6 +98,36 @@ function setUpEvents() {
             button.innerHTML = "Show Less";
         }
     }
+
+    var myMessage = document.getElementById("message");
+
+    function showMessage() {
+        myMessage.className = "show";
+    }
+
+    setTimeout(showMessage, 10);
+
+    var colorChanger = document.getElementById("color-changer");
+
+    var colors = ["red", "blue", "green", "pink"];
+    var counter = 0;
+
+    function changeColor() {
+
+        if(counter >= colors.length){
+            counter = 0;
+        }
+        colorChanger.style.background = colors[counter];
+        counter++;
+    
+    }
+
+    var myTimer = setInterval(changeColor, 3000);
+    
+    colorChanger.onclick = function(){
+        clearInterval(myTimer);
+        colorChanger.innerHTML = "Timer stopped";
+    }
 }
 
 window.onload = function () {
