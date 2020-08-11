caeaf62af9434ec68581a23125d557ac60e3fd59
Anna Stanek <aniastanekk@gmail.com>
2020-08-11 10:35:09 +0200

    Lesson 11 - Booleans

--- test.js
+++ test.js
@@ -14,8 +14,19 @@ myVariabe += 5; //maVariable = myVariale + 5
 ++ myVariabe; // first add 1, then print value of viariable
 myVariabe ++; //first print current value of variable, then add 1
 
-document.write (myVariabe); //print variable on the page
+document.write (myVariabe + '<br>'); //print variable on the page
 
 console.log(myVariabe); //print variable in consol
 
-document.write(20/5);
+document.write(20/5 + '<br>');
+
+var check = true;
+
+console.log(check);
+
+document.write((7<5) + '<br>') ;
+
+document.write('7>5 is: ' + Boolean(7>5)+ '<br>');
+document.write('0 is: ' + Boolean(0)+ '<br>');
+document.write('"hello" is: ' + Boolean("hello")+ '<br>');
+document.write('"" is: ' + Boolean("") + '<br>');
