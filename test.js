7e0a60a090dc809213cd8f093aca05bd3f8c998a
Anna Stanek <aniastanekk@gmail.com>
2020-08-11 09:14:22 +0200

    Lesson 9 - Math Operators Short-hand

--- test.js
+++ test.js
@@ -7,3 +7,9 @@ myVariabe = 10;
 var myVar ="hello"; //undefined
 
 myVariabe = myVariabe + "hello"; // "10hello"
+
+myVariabe += 5 //maVariable = myVariale + 5
+/** +=  -=  *=  /=  */
+
+++ myVariabe // first add 1, then print value of viariable
+myVariabe ++ //first print current value of variable, then add 1
