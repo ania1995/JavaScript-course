9ffd879660e1e5154aae74dd493e5711272dcdb8
Anna Stanek <aniastanekk@gmail.com>
2020-08-11 09:35:11 +0200

    Lesson 10 - Logging to the Console

--- test.js
+++ test.js
@@ -6,10 +6,16 @@ var myVariabe; //undefined
 myVariabe = 10;
 var myVar ="hello"; //undefined
 
-myVariabe = myVariabe + "hello"; // "10hello"
+//myVariabe = myVariabe + "hello"; // "10hello"
 
-myVariabe += 5 //maVariable = myVariale + 5
+myVariabe += 5; //maVariable = myVariale + 5
 /** +=  -=  *=  /=  */
 
-++ myVariabe // first add 1, then print value of viariable
-myVariabe ++ //first print current value of variable, then add 1
+++ myVariabe; // first add 1, then print value of viariable
+myVariabe ++; //first print current value of variable, then add 1
+
+document.write (myVariabe); //print variable on the page
+
+console.log(myVariabe); //print variable in consol
+
+document.write(20/5);
