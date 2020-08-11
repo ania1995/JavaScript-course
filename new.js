8e1a2b8c5a3a4fc3cd1ab78421462195bd3586f3
Anna Stanek <aniastanekk@gmail.com>
2020-08-11 12:19:01 +0200

    Lesson 22 - Working with Numbers

--- new.js
+++ new.js
@@ -8,10 +8,16 @@
 // && and
 // || or
 
-function getAverage(a, b, c){
-    var avarage = (a+b+c)/3
-    return avarage;
-}
+var a = 5;
+var b = 5;
+var c = "5";
 
-var myReseult = getAverage(4 ,6, 19.20);
-document.write("avarage is: " + myReseult);
+document.write( a + b + "<br> type of a+b is: " + typeof(a + b) + "<br><br>");
+document.write( a + c + "<br> type of a+c is: " + typeof(a + c) + "<br><br>");
+
+document.write( Math.round(7.5) + "<br>");
+document.write( Math.round(7.4) + "<br>");
+document.write( Math.floor(7.5) + "<br>");
+document.write( Math.ceil(7.5) + "<br>");
+document.write( Math.max(1 , 5 , 9 , 4 , 3) + "<br>");
+document.write( Math.PI + "<br>");
