4c896bd47ee3b663716c5233772cb5661979b766
Anna Stanek <aniastanekk@gmail.com>
2020-08-11 10:43:51 +0200

    Lesson 12 - If Statements

---
+++ lesson12.js
@@ -0,0 +1,13 @@
+var youLikeMeat = true;
+var myNum = 10;
+
+if (youLikeMeat){
+    document.write("Here is the meaty menu.... <br>");
+}
+
+if (myNum >= 10){
+    document.write("myNum is greater or egual to 10 <br>");
+} 
+else {
+    document.write("myNum is less then 10 <br>");
+}
