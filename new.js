5ac3e1e280c5b9739c555815bcb78ad75443c356
Anna Stanek <aniastanekk@gmail.com>
2020-08-11 11:07:10 +0200

    Lesson 15 - Logical Operators

--- new.js
+++ new.js
@@ -2,3 +2,17 @@
 // === compare value and type of variable
 // != not egual
 // !== compare value if is not egual
+// !=== compare value and type
+
+
+// && and
+// || or
+
+var myAge = 25;
+
+if (myAge >= 18 && myAge <= 30){
+    document.write("you can go to holiday");
+}
+else{
+    document.write("you can't go to holiday!");
+}
