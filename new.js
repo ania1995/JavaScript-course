77fd9be28ee7580ac9a2397ee1fb0b5bce2a621b
Anna Stanek <aniastanekk@gmail.com>
2020-08-11 22:26:17 +0200

    Lesson 40 - The OnClick Event

--- new.js
+++ new.js
@@ -74,11 +74,25 @@ event.onclick = function () {
     alert("hello!");
 };
 
-event.onmouseover = function(){
+event.onmouseover = function () {
     alert("you hovered your mouse over me")
 }
 
 //addEventListener();
 
 
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
 
+button.onclick = function () {
+
+    if (content.className == "open") {
+        // shrink the box
+        content.className = "";
+        button.innerHTML = "Show More";
+    } else {
+        //expand the box
+        content.className = "open";
+        button.innerHTML = "Show Less";
+    }
+}
